c12ec3f4475c43427753a88707557d62ce9db1fb
Ibrahima Sylla <isyll711@gmail.com>
2023-10-23 18:59:16 +0000

    Initial commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ecommerce',
+  description: "Super site d'achat et de vente sur Internet",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
